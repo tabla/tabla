381d19061e1642306fc2f051a445ea06a3c2136e
rsimoni <roberto.simoni@yahoo.it>
2013-08-30 16:08:33 +0200

    Implemented TableInMemory.update (and .first too)

--- tabla/src/main/java/tabla/RowBuilder.java
+++ tabla/src/main/java/tabla/RowBuilder.java
@@ -8,27 +8,32 @@ import java.util.Map;
  */
 public class RowBuilder<C extends ColumnsDefinition> {
 
-	public static <C extends ColumnsDefinition> RowBuilder<C> aRow(C columns) {
-		return new RowBuilder<C>(columns);
+	public static <C extends ColumnsDefinition> RowBuilder<C> aRow(C columnsDefinition) {
+		return new RowBuilder<C>(columnsDefinition);
 	}
 	
 	public RowBuilder<C> with(Column column, Object value) {
-		// FIXME validate that column is one of columns
+		// TODO validate that column is one of columns
 		values.put(column, value);
 		return this;
 	}
 	
+	public RowBuilder<C> with(Row<C> row) {
+		for (Column col : columnsDefinition.getColumns())
+			with(col, row.getValue(col));
+		return this;
+	}
+
 	public Row<C> toRow() {
 		return new Row<C>(values);
 	}
 	
-	private RowBuilder(C columns) {
-		this.columns = columns;
+	private RowBuilder(C columnsDefinition) {
+		this.columnsDefinition = columnsDefinition;
 		this.values = new LinkedHashMap<Column, Object>();
 	}
 	
-	@SuppressWarnings("unused")
-	private final C columns;
+	private final C columnsDefinition;
 	private final Map<Column, Object> values;
 	
 }
--- tabla/src/test/java/tabla/TableInMemoryTest.java
+++ tabla/src/test/java/tabla/TableInMemoryTest.java
@@ -35,6 +35,38 @@ public class TableInMemoryTest {
 		table.add(row);
 	}
 
+	@Test(expected=IllegalStateException.class)
+	public void update_throws_an_error_when_specified_row_does_not_exist() {
+		TableInMemory<CruiseColumnsDefinition> table = newTable();
+		Row<CruiseColumnsDefinition> row = RowBuilder.aRow(CruiseColumnsDefinition.DEFINITION)
+			.with(CruiseColumnsDefinition.DEPLOYMENT, "C")
+			.with(CruiseColumnsDefinition.CODE,       "12345678")
+			.toRow();
+		table.update(row);
+	}
+
+	@Test public void update_change_two_columns() {
+		TableInMemory<CruiseColumnsDefinition> table = newTable();
+		Row<CruiseColumnsDefinition> row = RowBuilder.aRow(CruiseColumnsDefinition.DEFINITION)
+			.with(CruiseColumnsDefinition.DEPLOYMENT,  "C")
+			.with(CruiseColumnsDefinition.CODE,        "12345678")
+			.with(CruiseColumnsDefinition.NAME,        "AAA")
+			.with(CruiseColumnsDefinition.DESCRIPTION, "aaa")
+			.toRow();
+		table.add(row);
+
+		Row<CruiseColumnsDefinition> newRow = RowBuilder.aRow(CruiseColumnsDefinition.DEFINITION)
+			.with(row)
+			.with(CruiseColumnsDefinition.NAME,        "BBB")
+			.with(CruiseColumnsDefinition.DESCRIPTION, "bbb")
+			.toRow();
+		table.update(newRow);
+		
+		assertEquals(1, table.count());
+		assertEquals("BBB", table.first().getValue(CruiseColumnsDefinition.NAME));
+		assertEquals("bbb", table.first().getValue(CruiseColumnsDefinition.DESCRIPTION));
+	}
+
 	private TableInMemory<CruiseColumnsDefinition> newTable() {
 		return new TableInMemory<CruiseColumnsDefinition>(CruiseColumnsDefinition.DEFINITION);
 	}
